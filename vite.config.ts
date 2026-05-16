213eafc4226f38d6b26313ebcd66e4a9c54f7867
corust-ci <ci@corust.ai>
2026-05-16 06:30:04 +0000

    feat: Initialize repo with Vite + TypeScript + pnpm

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 8080,
+  },
+})
